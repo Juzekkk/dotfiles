2ee18c37fb7afd5b4ec200dddfdef828312753ee
Juzekkk <juzekgaming@gmail.com>
2026-02-05 18:34:39 +0100

    feat(nvim/typst): init

---
+++ .config/nvim/lua/plugins/typst.lua
@@ -0,0 +1,52 @@
+return {
+  -- 1. Syntax Highlighting (Treesitter)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "typst" })
+      end
+    end,
+  },
+
+  -- 2. Ensure the tinymist binary is installed via Mason
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "tinymist" })
+    end,
+  },
+
+  -- 3. Configure the Tinymist LSP
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tinymist = {
+          -- Official configuration from https://myriad-dreamin.github.io/tinymist/frontend/neovim.html
+          single_file_support = true,
+          root_dir = function()
+            return vim.fn.getcwd()
+          end,
+          settings = {
+            exportPdf = "onType", -- Export PDF on type (options: "onType", "onSave", "never")
+            outputPath = "$root/target/$dir/$name", -- output to target directory
+            formatterMode = "typstyle", -- Use 'typstyle' or 'typstfmt'
+          },
+        },
+      },
+    },
+  },
+
+  -- 4. Add a previewer (highly recommended for Typst)
+  {
+    "chomosuke/typst-preview.nvim",
+    ft = "typst",
+    version = "1.*",
+    build = function() require("typst-preview").update() end,
+    keys = {
+      { "<leader>tp", "<cmd>TypstPreview<cr>", desc = "Start Typst Preview" },
+    },
+  },
+}
--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -12,6 +12,7 @@ return {
 
     require("which-key").add {
       -- <leader>s is a prefix for uncommon pickers, unlike live_grep and find_files
+      { "<leader>t", group = "Typst" },
       { "<leader>m", group = "EasyTable"},
       { "<leader>s", group = "Search" },
       { "<leader>h", group = "Hunks", icon = { icon = "", color = "red" } },
