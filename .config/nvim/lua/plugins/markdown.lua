a197d4d94996d320795069a3b092f597a1a7c6cb
Juzekkk <juzekgaming@gmail.com>
2026-02-07 20:33:52 +0100

    feat(nvim/markdown): moved from in file preview to preview as a site

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,65 +1,66 @@
 return {
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons", -- or "echasnovski/mini.icons"
-    },
-    ft = { "markdown", "Avante" },
-    opts = {
-        link = {
-            enabled = false -- don't show icon
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = "cd app && npm install",
+        keys = {
+            { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Start Markdown Preview" },
         },
-        pipe_table = {
-            present = 'round',
-            border_virtual = true,
-        },
-      -- You can customize your config here.
+        init = function()
+            vim.cmd([[
+                function OpenMarkdownPreview (url)
+                    " shellescape() protects against URLs with special chars
+                    " The '&' allows Neovim to keep running the server
+                    execute "silent ! firefox -P typst-preview -new-window " . shellescape(a:url) . " &"
+                endfunction
+            ]])
+            vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
+        end,
     },
-  },
-  {
-      "Myzel394/easytables.nvim",
-      ft = "markdown",
-      -- 'keys' belongs to lazy.nvim, not inside setup()
-      keys = {
-          { "<Leader>mtn", ":EasyTablesCreateNew ", desc = "Create New Table (e.g. 5x4)" },
-          { "<Leader>mti", "<cmd>EasyTablesImportThisTable<CR>", desc = "Import/Edit Current Table" },
-      },
-      config = function()
-          require("easytables").setup({
-              -- 'set_mappings' belongs directly in setup(), not inside a nested 'opts'
-              set_mappings = function(buf)
-                  local map = function(lhs, rhs, desc)
-                      vim.api.nvim_buf_set_keymap(buf, "n", lhs, rhs, {
-                          nowait = true,
-                          silent = true,
-                          desc = "EasyTables: " .. desc,
-                      })
-                  end
+    {
+        "Myzel394/easytables.nvim",
+        ft = "markdown",
+        -- 'keys' belongs to lazy.nvim, not inside setup()
+        keys = {
+            { "<Leader>mtn", ":EasyTablesCreateNew ", desc = "Create New Table (e.g. 5x4)" },
+            { "<Leader>mti", "<cmd>EasyTablesImportThisTable<CR>", desc = "Import/Edit Current Table" },
+        },
+        config = function()
+            require("easytables").setup({
+                -- 'set_mappings' belongs directly in setup(), not inside a nested 'opts'
+                set_mappings = function(buf)
+                    local map = function(lhs, rhs, desc)
+                        vim.api.nvim_buf_set_keymap(buf, "n", lhs, rhs, {
+                            nowait = true,
+                            silent = true,
+                            desc = "EasyTables: " .. desc,
+                        })
+                    end
 
-                  -- HJKL Navigation
-                  map("h", ":JumpLeft<CR>", "Jump Left")
-                  map("l", ":JumpRight<CR>", "Jump Right")
-                  map("k", ":JumpUp<CR>", "Jump Up")
-                  map("j", ":JumpDown<CR>", "Jump Down")
+                    -- HJKL Navigation
+                    map("h", ":JumpLeft<CR>", "Jump Left")
+                    map("l", ":JumpRight<CR>", "Jump Right")
+                    map("k", ":JumpUp<CR>", "Jump Up")
+                    map("j", ":JumpDown<CR>", "Jump Down")
 
-                  -- Shift + HJKL for Swapping Cells
-                  map("H", ":SwapWithLeftCell<CR>", "Swap Left")
-                  map("L", ":SwapWithRightCell<CR>", "Swap Right")
-                  map("K", ":SwapWithUpperCell<CR>", "Swap Up")
-                  map("J", ":SwapWithLowerCell<CR>", "Swap Down")
+                    -- Shift + HJKL for Swapping Cells
+                    map("H", ":SwapWithLeftCell<CR>", "Swap Left")
+                    map("L", ":SwapWithRightCell<CR>", "Swap Right")
+                    map("K", ":SwapWithUpperCell<CR>", "Swap Up")
+                    map("J", ":SwapWithLowerCell<CR>", "Swap Down")
 
-                  -- Ctrl + HJKL for Swapping Rows/Columns
-                  map("<C-h>", ":SwapWithLeftColumn<CR>", "Swap Col Left")
-                  map("<C-l>", ":SwapWithRightColumn<CR>", "Swap Col Right")
-                  map("<C-k>", ":SwapWithUpperRow<CR>", "Swap Row Up")
-                  map("<C-j>", ":SwapWithLowerRow<CR>", "Swap Row Down")
+                    -- Ctrl + HJKL for Swapping Rows/Columns
+                    map("<C-h>", ":SwapWithLeftColumn<CR>", "Swap Col Left")
+                    map("<C-l>", ":SwapWithRightColumn<CR>", "Swap Col Right")
+                    map("<C-k>", ":SwapWithUpperRow<CR>", "Swap Row Up")
+                    map("<C-j>", ":SwapWithLowerRow<CR>", "Swap Row Down")
 
-                  -- Standard Tab navigation
-                  map("<Tab>", ":JumpToNextCell<CR>", "Next Cell")
-                  map("<S-Tab>", ":JumpToPreviousCell<CR>", "Prev Cell")
-              end,
-          })
-      end,
-  },
+                    -- Standard Tab navigation
+                    map("<Tab>", ":JumpToNextCell<CR>", "Next Cell")
+                    map("<S-Tab>", ":JumpToPreviousCell<CR>", "Prev Cell")
+                end,
+            })
+        end,
+    },
 }
