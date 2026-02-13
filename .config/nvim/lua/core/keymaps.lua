39bfe2de222c66fbe809c00f14de3d8540c03884
Juzekkk <juzekgaming@gmail.com>
2026-02-13 09:01:48 +0100

    feat(nvim): added move-one-left mapping for insert mode

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -12,5 +12,8 @@ keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
 keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
 keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
 
+-- Insert Mode Mappings
+keymap.set("i", "<C-;>", "<C-o>l", { desc = "Move one character left" })
+
 -- Exit insert mode in terminal --
 -- vim.keymap.set("t", "<C-[>", [[<C-\><C-n>]], { noremap = true, silent = true })
